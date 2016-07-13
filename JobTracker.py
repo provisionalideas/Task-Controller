bf14b05f676457b8895e8defcb1d8ec92574fbb2
James Birch <jamesbirch@Jamess-iMac.local>
2016-07-12 20:04:17 -0400

    set up rudimentary task list

--- JobTracker.py
+++ JobTracker.py
@@ -31,81 +31,84 @@ atexit.register(timer)
 
 # def NewTask():
 
-
-print 'JOB COUNTER:'
-print 'GOALS:'
-print '- 5 programming (1), 10 tutoring (2), 5 healthcare (3)'
-print '- 5 temp jobs (4), 5 restaurants (5)'
-print 'TOTAL:       [                               ] 0'
-print 'PROGRAMMING: [                               ] 0'
-print 'TUTORING:    [                               ] 0'
-print 'HEALTHCARE:  [                               ] 0'
-print 'TEMP:        [                               ] 0'
-print 'RESTAURANTS: [                               ] 0'
-
 prompt = ''
 
 while (prompt != 'EXIT'):
-    prompt = raw_input('Which have you filed? ')
 
-    if prompt == '1':
-        codejob += 1
-        os.system('clear')
-    elif prompt == '2':
-        tutorjob += 1
-        os.system('clear')
-    elif prompt == '3':
-        hcjob += 1
-        os.system('clear')
-    elif prompt == '4':
-        tempjob += 1
-        os.system('clear')
-    elif prompt == '5':
-        restojob += 1
-        os.system('clear')
+    prompt = raw_input('Enter your command. ')
+    os.system('clear')
+
+    if prompt == "Apply to jobs.":
+        while prompt in ['Apply to jobs.','1','2','3','4','5']:
+
+            print "~~JAMES' JOB APPLICATION TRACKER:~~"
+            #TODO ADD GOAL SETTING MODULE
+            print 'GOALS:'
+            print '- 5 programming (1), 10 tutoring (2), 5 healthcare (3)'
+            print '- 5 temp jobs (4), 5 restaurants (5)'
+
+            print 'PROGRAMMING: [',
+            for i in range(0,codejob):
+                print "|||||",
+            for i in range(0,(5-codejob)):
+                print "     ",
+            print "]",codejob
+
+            print 'TUTORING:    [',
+            for i in range(0,tutorjob):
+                print "||",
+            for i in range(0,(10-tutorjob)):
+                print "  ",
+            print "]",tutorjob
+
+            print 'HEALTHCARE:  [',
+            for i in range(0,hcjob):
+                print "|||||",
+            for i in range(0,(5-hcjob)):
+                print "     ",
+            print "]",hcjob
+
+            print 'TEMP:        [',
+            for i in range(0,tempjob):
+                print "|||||",
+            for i in range(0,(5-tempjob)):
+                print "     ",
+            print "]",tempjob
+
+            print 'RESTAURANTS: [',
+            for i in range(0,restojob):
+                print "|||||",
+            for i in range(0,(5-restojob)):
+                print "     ",
+            print "]",restojob
+
+            print "TOTAL:       [",
+            print '|' * (codejob+tutorjob+hcjob+tempjob+restojob),
+            print ' ' * (28-(codejob+tutorjob+hcjob+tempjob+restojob)),']',(codejob+tutorjob+hcjob+tempjob+restojob)
+
+            prompt = raw_input('Which have you filed? ')
+
+            if prompt == '1':
+                codejob += 1
+            elif prompt == '2':
+                tutorjob += 1
+            elif prompt == '3':
+                hcjob += 1
+            elif prompt == '4':
+                tempjob += 1
+            elif prompt == '5':
+                restojob += 1
+
+            #TODO set dynamic number of job types, autonomously set goals, etc. in goal setting component.
+
+            os.system('clear')
+
+            if prompt in ['Apply to jobs.','1','2','3','4','5']:
+                random.seed()
+                print cheers[random.randint(0,6)]
+
     elif prompt[:3] == 'add':
         TaskList.insert(0,prompt[4:])
-        print TaskList
-
-    print "~~JAMES' JOB APPLICATION TRACKER:~~"
-    print 'PROGRAMMING: [',
-    for i in range(0,codejob):
-        print "|||||",
-    for i in range(0,(5-codejob)):
-        print "     ",
-    print "]",codejob
-
-    print 'TUTORING:    [',
-    for i in range(0,tutorjob):
-        print "||",
-    for i in range(0,(10-tutorjob)):
-        print "  ",
-    print "]",tutorjob
-
-    print 'HEALTHCARE:  [',
-    for i in range(0,hcjob):
-        print "|||||",
-    for i in range(0,(5-hcjob)):
-        print "     ",
-    print "]",hcjob
-
-    print 'TEMP:        [',
-    for i in range(0,tempjob):
-        print "|||||",
-    for i in range(0,(5-tempjob)):
-        print "     ",
-    print "]",tempjob
-
-    print 'RESTAURANTS: [',
-    for i in range(0,restojob):
-        print "|||||",
-    for i in range(0,(5-restojob)):
-        print "     ",
-    print "]",restojob
-
-    print "TOTAL:       [",
-    print '|' * (codejob+tutorjob+hcjob+tempjob+restojob),
-    print ' ' * (28-(codejob+tutorjob+hcjob+tempjob+restojob)),']',(codejob+tutorjob+hcjob+tempjob+restojob)
-
-    random.seed()
-    print cheers[random.randint(0,6)]
+        print "TASK LIST:"
+        for a,b in enumerate(TaskList,1):
+            print '{} {}'.format(a,b)
