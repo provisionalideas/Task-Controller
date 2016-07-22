360ef4cb6ade47ff6c925c32082c9679d022834d
provisionalideas <provisionalideas@gmail.com>
2016-07-22 16:32:36 -0400

    Fixed savelocation, added a bunch of todos

--- JobTracker.py
+++ JobTracker.py
@@ -8,11 +8,28 @@ tutorjob = 0
 hcjob = 0
 tempjob = 0
 restojob = 0
+toggle = 0
+options = []
+
+saveLocation = os.path.expanduser("~") + "/WorkTracker"
+if not os.path.exists(saveLocation):
+    os.makedirs(saveLocation)
+saveLocation = saveLocation + "/TaskList.txt"
+if not os.path.exists(saveLocation):
+    storage = open(saveLocation,'w+')
+    storage.close()
 
 TaskList = []
-storage = open('TaskList.txt','r')
+ProjectList = []
+
+storage = open(saveLocation,'r+')
 for line in storage:
-    TaskList.append(line)
+    if ("-*-*-*-*-*-*-*-*-*-*-*-*-" not in line) and (toggle == 0):
+        TaskList.append(line)
+    elif "-*-*-*-*-*-*-*-*-*-*-*-*-" in line:
+        toggle = 1
+    else:
+        ProjectList.append(line)
 storage.close()
 
 cheers = ['Good job!','Keep going!',"You're doing well!","Keep it up!","Yaaaay!",
@@ -32,12 +49,27 @@ def timer():
     print ("--- %s hours, %s minutes, %s seconds ---" % (hours, minutes, seconds))
 
 def savelist():
-    storage = open('TaskList.txt','w+')
+    storage = open(saveLocation,'w+')
     storage.truncate()
     for item in TaskList:
         storage.write(item)
         storage.write("\n")
+    storage.write("-*-*-*-*-*-*-*-*-*-*-*-*-\n")
+    for item in ProjectList:
+        storage.write(item)
+        storage.write("\n")
     storage.close()
+
+def printTodo(toggle):
+    if toggle == 'tasks':
+        print "TASK LIST:"
+        for a,b in enumerate(TaskList,1):
+            print '{} {}'.format(a,b)
+    elif toggle == 'projects':
+        print "ACTIVE PROJECTS:"
+        for a,b in enumerate(ProjectList,1):
+            print '{} {}'.format(a,b)
+
 
 atexit.register(timer)
 atexit.register(savelist)
@@ -46,6 +78,7 @@ prompt = ''
 
 while (prompt != 'EXIT') and (prompt != 'exit'):
 #TODO Fix to be case-insensitive
+    options = []
     prompt = raw_input('Enter your command. ')
     os.system('clear')
 
@@ -57,6 +90,16 @@ while (prompt != 'EXIT') and (prompt != 'exit'):
             print 'GOALS:'
             print '- 5 programming (1), 10 tutoring (2), 5 healthcare (3)'
             print '- 5 temp jobs (4), 5 restaurants (5)'
+
+            # if "goal" in prompt:
+            #TODO BUILD GOALSETTING MODULE
+
+            # biggest = 0
+            # for item in targetList:
+            #     if len(item) > biggest:
+            #         biggest = len(item)
+            # print goal + ": " + (len(biggest) - len(goal))*' ' + "[",
+
 
             print 'PROGRAMMING: [',
             for i in range(0,codejob):
@@ -118,33 +161,123 @@ while (prompt != 'EXIT') and (prompt != 'exit'):
                 random.seed()
                 print cheers[random.randint(0,6)]
 
-    elif prompt[:3] == 'add':
-        TaskList.insert(0,prompt[4:])
-        print "TASK LIST:"
-        for a,b in enumerate(TaskList,1):
-            print '{} {}'.format(a,b)
+    elif prompt[:3] == 'add' or prompt[:3] == 'new':
+
+        #TODO make alternative for "add new project"
+        if 'project' in prompt[4:12]:
+            ProjectList.insert(0,prompt[12:])
+            printTodo('projects')
+
+        elif 'task' in prompt[4:9]:
+            TaskList.insert(0,prompt[9:])
+            printTodo('tasks')
+
+        else:
+            storage = prompt[4:]
+            prompt = raw_input("Task or project? ")
+            if prompt == 'task':
+                TaskList.insert(0,storage)
+                printTodo('tasks')
+            elif prompt == 'project':
+                ProjectList.insert(0,storage)
+                printTodo('projects')
+
+                #TODO what to do if someone puts something else in?
+                #keyword wcould also be 'task', 'project', 'item', etc.
+                #TODO make a help command system
+
+        #TODO Make everything case-insensitive
+
+    #TODO naturally learn whether something is a task or a project
 
     elif (prompt[:9] == 'completed') or (prompt[:8] == 'finished'):
+        print prompt[:9]
         if prompt[:9] == 'completed':
             for item in TaskList:
                 if prompt[10:] in item:
                     print item
+                    options.append(item)
+            if len(options) == 1:
+                TaskList.remove(prompt[10:])
         elif prompt[:8] == 'finished':
             for item in TaskList:
                 if prompt[9:] in item:
                     print item
+                    options.append(item)
 
+                    #OPTIONS....
+            if len(options) == 1:
+                TaskList.remove(prompt[9:])
+        print options
+        prompt = raw_input("TEST ")
+
+        #TODO add task removal component
+        #TODO remove item from task or project LIST
+        #TODO create removal for project list
+        #TODO add in announcements like in Countdown Timer
 
     elif prompt[:4] == 'list':
-        print "TASK LIST:"
-        for a,b in enumerate(TaskList,1):
-            print '{} {}'.format(a,b)
+        if len(prompt) > 4:
+            print prompt[5]
+            if prompt[5] == 'p':
+                printTodo('projects')
+            elif prompt[5] == 't':
+                printTodo('tasks')
+            else:
+                printTodo('projects')
+                printTodo('tasks')
+        else:
+            printTodo('projects')
+            printTodo('tasks')
+
+    elif prompt[:10] == 'prioritize':
+        printTodo('tasks')
+        prompt = raw_input("set new order. ")
+        prompt = prompt.split(",")
+        TaskList = [ TaskList[i] for i in prompt]
+        printTodo('tasks')
+
+    #tasks, todo, --> find first space, then compare item before space to dicitonary?
+    #projects, active projects, current projects
+
+    elif prompt[:5] == 'start':
+        if len(prompt) > 5:
+            for item in TaskList:
+                if prompt[6:] in item:
+                    options.append(item)
+                    if len(options) > 1:
+                        for a,b in enumerate(ProjectList,1):
+                            print '{} {}'.format(a,b)
+                        prompt = raw_input("Which task do you want to start? ")
+                        counter = 0
+                        for item in options:
+                            if prompt in item:
+                                counter = counter + 1
+                        if counter != 1:
+                            print "try again"
+                        else:
+                            print "finish this part"
+        else:
+            printTodo('tasks')
+            prompt = raw_input("Which task do you want to start? ")
+
+            #TODO enable search by index
+            #TODO start by
+
+    #TODO accord tasks to projects
+    #TODO task list prioritization engine
+    #TODO online syncing of projects and tasks across machine via login
+
+    #TODO add responsive follow-throughs -- i.e. add command after proj --> add proj.
 
     elif prompt[:5] == 'clear':
         TaskList = []
-        storage = open('TaskList.txt','w+')
+        ProjectList = []
+        storage = open(saveLocation,'w+')
         storage.truncate()
         storage.close()
+    #TODO IMPROVE CLEAR SYSTEM SO YOU CAN SELECT CLEARING TASK LIST OR PROJECTS LIST OR BOTH
 
         #TODO improve list storage system, change storage location
         #TODO add mechanism to remove completed items from list
+        #TODO build task timer
